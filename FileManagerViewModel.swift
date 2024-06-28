2a846069ea59eb7646bdbe2580fb62c05cc95bff
speedyfriend433 <speedyfriend13@gmail.com>
2024-06-28 15:54:08 +0900

    Update FileManagerViewModel.swift

--- FileManagerViewModel.swift
+++ FileManagerViewModel.swift
@@ -33,7 +33,6 @@ class FileManagerViewModel: ObservableObject {
         }
     }
     @Published var isSearching: Bool = false
-    @Published var trashItems: [FileSystemItem] = []
     var directory: URL
     private let fileManager = FileManager.default
     private var rootSearchCancellable: AnyCancellable?
@@ -44,25 +43,22 @@ class FileManagerViewModel: ObservableObject {
     }
 
     func loadFiles() {
-        isSearching = true
-        rootSearchCancellable?.cancel()
-        rootSearchCancellable = Future<[FileSystemItem], Error> { promise in
-            DispatchQueue.global(qos: .userInitiated).async {
-                let rootURL = URL(fileURLWithPath: "/")
-                let rootItems = self.recursiveFileSearch(at: rootURL)
-                promise(.success(rootItems))
+        do {
+            let directoryContents = try fileManager.contentsOfDirectory(at: directory, includingPropertiesForKeys: [.isDirectoryKey, .fileSizeKey, .creationDateKey, .contentModificationDateKey], options: [.skipsHiddenFiles])
+            DispatchQueue.main.async {
+                self.items = directoryContents.map { url in
+                    let resourceValues = try? url.resourceValues(forKeys: [.isDirectoryKey, .fileSizeKey, .creationDateKey, .contentModificationDateKey])
+                    let isDirectory = resourceValues?.isDirectory ?? false
+                    let fileSize = resourceValues?.fileSize ?? 0
+                    let creationDate = resourceValues?.creationDate ?? Date()
+                    let modificationDate = resourceValues?.contentModificationDate ?? Date()
+                    return FileSystemItem(name: url.lastPathComponent, isDirectory: isDirectory, path: url, size: fileSize, creationDate: creationDate, modificationDate: modificationDate)
+                }
+                self.sortItems()
+                self.filterItems()
             }
-        }
-        .receive(on: DispatchQueue.main)
-        .sink { completion in
-            if case .failure(let error) = completion {
-                print("Failed to search root files: \(error.localizedDescription)")
-                self.isSearching = false
-            }
-        } receiveValue: { rootItems in
-            self.rootItems = rootItems
-            self.isSearching = false
-            self.filterItems()
+        } catch {
+            print("Failed to load files: \(error.localizedDescription)")
         }
     }
 
@@ -97,15 +93,14 @@ class FileManagerViewModel: ObservableObject {
     private func recursiveFileSearch(at url: URL) -> [FileSystemItem] {
         var result: [FileSystemItem] = []
         do {
-            let contents = try fileManager.contentsOfDirectory(at: url, includingPropertiesForKeys: [.isDirectoryKey, .fileSizeKey, .creationDateKey, .contentModificationDateKey, .isSymbolicLinkKey], options: [.skipsHiddenFiles])
+            let contents = try fileManager.contentsOfDirectory(at: url, includingPropertiesForKeys: [.isDirectoryKey, .fileSizeKey, .creationDateKey, .contentModificationDateKey], options: [.skipsHiddenFiles])
             for item in contents {
-                let resourceValues = try? item.resourceValues(forKeys: [.isDirectoryKey, .fileSizeKey, .creationDateKey, .contentModificationDateKey, .isSymbolicLinkKey])
+                let resourceValues = try? item.resourceValues(forKeys: [.isDirectoryKey, .fileSizeKey, .creationDateKey, .contentModificationDateKey])
                 let isDirectory = resourceValues?.isDirectory ?? false
-                let isSymlink = resourceValues?.isSymbolicLink ?? false
                 let fileSize = resourceValues?.fileSize ?? 0
                 let creationDate = resourceValues?.creationDate ?? Date()
                 let modificationDate = resourceValues?.contentModificationDate ?? Date()
-                let fileSystemItem = FileSystemItem(name: item.lastPathComponent, isDirectory: isDirectory, url: item, size: fileSize, creationDate: creationDate, modificationDate: modificationDate, isSymlink: isSymlink)
+                let fileSystemItem = FileSystemItem(name: item.lastPathComponent, isDirectory: isDirectory, path: item, size: fileSize, creationDate: creationDate, modificationDate: modificationDate)
                 if fileSystemItem.name.lowercased().contains(searchQuery.lowercased()) {
                     result.append(fileSystemItem)
                 }
@@ -161,7 +156,7 @@ class FileManagerViewModel: ObservableObject {
     func deleteSelectedFiles() {
         for id in selectedItems {
             if let item = items.first(where: { $0.id == id }) {
-                deleteFile(at: item.url)
+                deleteFile(at: item.path)
             }
         }
         selectedItems.removeAll()
@@ -209,57 +204,10 @@ class FileManagerViewModel: ObservableObject {
         }
     }
 
-    func moveFileToTrash(at url: URL) {
-        let trashURL = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(url.lastPathComponent)
-        do {
-            try fileManager.moveItem(at: url, to: trashURL)
-            loadFiles()
-            loadTrash()
-        } catch {
-            print("Failed to move file to trash: \(error.localizedDescription)")
-        }
-    }
-
-    func loadTrash() {
-        let trashURL = URL(fileURLWithPath: NSTemporaryDirectory())
-        do {
-            let trashContents = try fileManager.contentsOfDirectory(at: trashURL, includingPropertiesForKeys: [.isDirectoryKey, .fileSizeKey, .creationDateKey, .contentModificationDateKey], options: [.skipsHiddenFiles])
-            DispatchQueue.main.async {
-                self.trashItems = trashContents.map { url in
-                    let resourceValues = try? url.resourceValues(forKeys: [.isDirectoryKey, .fileSizeKey, .creationDateKey, .contentModificationDateKey])
-                    let isDirectory = resourceValues?.isDirectory ?? false
-                    let fileSize = resourceValues?.fileSize ?? 0
-                    let creationDate = resourceValues?.creationDate ?? Date()
-                    let modificationDate = resourceValues?.contentModificationDate ?? Date()
-                    return FileSystemItem(name: url.lastPathComponent, isDirectory: isDirectory, url: url, size: fileSize, creationDate: creationDate, modificationDate: modificationDate, isSymlink: false)
-                }
-            }
-        } catch {
-            print("Failed to load trash: \(error.localizedDescription)")
-        }
-    }
-
-    func emptyTrash() {
-        let trashURL = URL(fileURLWithPath: NSTemporaryDirectory())
-        do {
-            let trashContents = try fileManager.contentsOfDirectory(at: trashURL, includingPropertiesForKeys: nil, options: [])
-            for item in trashContents {
-                try fileManager.removeItem(at: item)
-            }
-            loadTrash()
-        } catch {
-            print("Failed to empty trash: \(error.localizedDescription)")
-        }
-    }
-
-    func restoreFromTrash(_ item: FileSystemItem) {
-        let restoredURL = directory.appendingPathComponent(item.name)
-        do {
-            try fileManager.moveItem(at: item.url, to: restoredURL)
-            loadTrash()
-            loadFiles()
-        } catch {
-            print("Failed to restore item from trash: \(error.localizedDescription)")
-        }
+    func formattedFileSize(_ size: Int) -> String {
+        let formatter = ByteCountFormatter()
+        formatter.allowedUnits = [.useKB, .useMB, .useGB]
+        formatter.countStyle = .file
+        return formatter.string(fromByteCount: Int64(size))
     }
 }
