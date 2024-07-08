b2f3deda4f215803fd8a3d3a02553b313b44d74b
speedyfriend433 <speedyfriend13@gmail.com>
2024-07-08 14:19:37 +0900

    Update main.swift

--- RootHelper/main.swift
+++ RootHelper/main.swift
@@ -5,96 +5,91 @@
 //
 import Foundation
 
-func sendOutThisMessage() {
+func help() {
     print("""
     Usage: RootHelper [action] [path]
-    Made by Le Bao Nguyen (@lebao3105 on GitHub and GitLab)
-    Written in C++ and Pascal (2 different versions). Write once, run almost everywhere!
-    (This C++ thing, in fact, mostly uses C things.)
+    Proudly written in Swift. Write once, run everywhere!
 
     Available [action]s:
     del / d [path]                             : Deletes [path]
     list / l [path]                            : Shows the content [path]
     create / c [path]                          : Creates [path]
     createdir / md [path]                      : Creates [path] as a directory
-    move / mv [path, list must not be odd]     : Moves files and folders
-    copy / cp [path, list must not be odd]     : Copies files and folders to another location
+    move / mv [path, list must not be odd]     : Moves files (FILES only for now)
+    copy / cp [path, list must not be odd]     : Copies files (FILES only)
     getuid / guid                              : Gets and shows the current UID
     getgid / gid                               : Gets and shows the current GID
 
-    [path] can in any number of absolute paths that the program and system can handle.
+    [path] can be any number of absolute paths that the program and system can handle, in any kind: file and folder.
 
-    Warning: NO confirmation message. No success/progress message. You've been warned.
+    This is MEANT to be used INTERNALLY by Swifile - a File manager.
+    Any damages to the file system by you using this? You're the one who is responsible for it, not us.
+    This project is a part of Swifile, licensed under the MIT license.
     """)
 }
 
-func isMod2Equal0(_ argc: Int) throws {
-    if argc % 2 != 0 {
-        throw NSError(domain: "Not enough arguments!", code: 1, userInfo: nil)
+func moveItems(_ args: [String]) throws {
+    guard (args.count - 1) % 2 == 0 else {
+        throw FileOperationError.notEnoughArguments
+    }
+    for i in stride(from: 1, to: args.count, by: 2) {
+        try FileOperations.moveItem(from: args[i], to: args[i + 1])
     }
 }
 
-func checkArg(_ arg: String, longarg: String, shortarg: String) -> Bool {
-    return arg == longarg || arg == shortarg
-}
-
-func enoughArgs(_ argc: Int) throws {
-    if argc <= 2 {
-        sendOutThisMessage()
-        throw NSError(domain: "Not enough arguments!", code: 1, userInfo: nil)
+func copyItems(_ args: [String]) throws {
+    guard (args.count - 1) % 2 == 0 else {
+        throw FileOperationError.notEnoughArguments
+    }
+    for i in stride(from: 1, to: args.count, by: 2) {
+        try FileOperations.copyFile(from: args[i], to: args[i + 1])
     }
 }
 
 func main() {
-    let argc = Int(CommandLine.argc)
-    let argv = CommandLine.arguments
+    let args = CommandLine.arguments
 
-    do {
-        if checkArg(argv[1], longarg: "del", shortarg: "d") {
-            try enoughArgs(argc)
-            for i in 2..<argc {
-                try FileManager.default.removeItem(atPath: argv[i])
-            }
-        } else if checkArg(argv[1], longarg: "list", shortarg: "l") {
-            try enoughArgs(argc)
-            for i in 2..<argc {
-                let contents = try FileManager.default.contentsOfDirectory(atPath: argv[i])
-                for content in contents {
-                    print(content)
+    if args.count == 1 {
+        help()
+        exit(0)
+    } else {
+        do {
+            switch args[1] {
+            case "del", "d":
+                for i in 2..<args.count {
+                    try FileOperations.removeItem(at: args[i])
                 }
+            case "list", "l":
+                for i in 2..<args.count {
+                    try FileOperations.contentsOfDirectory(at: args[i])
+                    print()
+                }
+            case "create", "c":
+                for i in 2..<args.count {
+                    try FileOperations.createItem(at: args[i])
+                }
+            case "createdir", "md":
+                for i in 2..<args.count {
+                    try FileManager.default.createDirectory(atPath: args[i], withIntermediateDirectories: false, attributes: nil)
+                }
+            case "move", "mv":
+                try moveItems(Array(args[2...]))
+            case "copy", "cp":
+                try copyItems(Array(args[2...]))
+            case "getuid", "guid":
+                print("UID: \(getuid())")
+                exit(0)
+            case "getgid", "gid":
+                print("GID: \(getgid())")
+                exit(0)
+            default:
+                help()
+                throw FileOperationError.unknownAction
             }
-        } else if checkArg(argv[1], longarg: "create", shortarg: "c") {
-            try enoughArgs(argc)
-            for i in 2..<argc {
-                FileManager.default.createFile(atPath: argv[i], contents: Data(), attributes: nil)
-            }
-        } else if checkArg(argv[1], longarg: "createdir", shortarg: "md") {
-            try enoughArgs(argc)
-            for i in 2..<argc {
-                try FileManager.default.createDirectory(atPath: argv[i], withIntermediateDirectories: false, attributes: nil)
-            }
-        } else if checkArg(argv[1], longarg: "move", shortarg: "mv") {
-            try enoughArgs(argc)
-            try isMod2Equal0(argc)
-            for i in stride(from: 2, to: argc, by: 2) {
-                try FileManager.default.moveItem(atPath: argv[i], toPath: argv[i + 1])
-            }
-        } else if checkArg(argv[1], longarg: "copy", shortarg: "cp") {
-            try enoughArgs(argc)
-            try isMod2Equal0(argc)
-            for i in stride(from: 2, to: argc, by: 2) {
-                try FileManager.default.copyItem(atPath: argv[i], toPath: argv[i + 1])
-            }
-        } else if checkArg(argv[1], longarg: "getuid", shortarg: "guid") {
-            print("UID: \(getuid())")
-        } else if checkArg(argv[1], longarg: "getgid", shortarg: "gid") {
-            print("GID: \(getgid())")
-        } else {
-            sendOutThisMessage()
-            throw NSError(domain: "Unknown command used. Quit now.", code: 1, userInfo: nil)
+        } catch {
+            print("Error: \(error)")
+            exit(1)
         }
-    } catch {
-        print(error.localizedDescription)
     }
 }
 
