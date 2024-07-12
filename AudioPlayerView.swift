3b745944fa63db6c2d050f96ad7fb863f31e2282
speedyfriend433 <speedyfriend13@gmail.com>
2024-07-12 12:56:54 +0900

    Update AudioPlayerView.swift

--- AudioPlayerView.swift
+++ AudioPlayerView.swift
@@ -10,8 +10,14 @@ import AVFoundation
 struct AudioPlayerView: UIViewControllerRepresentable {
     let fileURL: URL
 
-    class Coordinator: NSObject {
+    class Coordinator: NSObject, AVAudioPlayerDelegate {
         var player: AVAudioPlayer?
+        var timer: Timer?
+        var parent: AudioPlayerView
+
+        init(parent: AudioPlayerView) {
+            self.parent = parent
+        }
 
         @objc func playPauseAudio() {
             guard let player = player else { return }
@@ -19,12 +25,35 @@ struct AudioPlayerView: UIViewControllerRepresentable {
                 player.pause()
             } else {
                 player.play()
+                startTimer()
             }
+        }
+
+        @objc func updateProgress() {
+            guard let player = player else { return }
+            parent.progress = player.currentTime / player.duration
+        }
+
+        func startTimer() {
+            timer?.invalidate()
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateProgress), userInfo: nil, repeats: true)
+        }
+
+        func stopTimer() {
+            timer?.invalidate()
+            timer = nil
+        }
+
+        func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+            stopTimer()
+            parent.progress = 0
         }
     }
 
+    @Binding var progress: Double
+
     func makeCoordinator() -> Coordinator {
-        Coordinator()
+        Coordinator(parent: self)
     }
 
     func makeUIViewController(context: Context) -> UIViewController {
@@ -44,6 +73,7 @@ struct AudioPlayerView: UIViewControllerRepresentable {
         DispatchQueue.global(qos: .userInitiated).async {
             do {
                 let player = try AVAudioPlayer(contentsOf: self.fileURL)
+                player.delegate = context.coordinator
                 player.prepareToPlay()
                 DispatchQueue.main.async {
                     context.coordinator.player = player
@@ -61,6 +91,6 @@ struct AudioPlayerView: UIViewControllerRepresentable {
 
 struct AudioPlayerView_Previews: PreviewProvider {
     static var previews: some View {
-        AudioPlayerView(fileURL: URL(fileURLWithPath: "/path/to/audio.mp3"))
+        AudioPlayerView(fileURL: URL(fileURLWithPath: "/path/to/audio.mp3"), progress: .constant(0))
     }
 }
